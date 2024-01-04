4fd4f5aa527f27e005a19378710ab3f7fcf2ceb0
imhasnat <imhasnat00@gmail.com>
2024-01-04 18:52:25 +0600

    Dynamic Why Choose Us complete

--- ui/src/components/HomeWhyUs.js
+++ ui/src/components/HomeWhyUs.js
@@ -1,9 +1,9 @@
-import { Text } from '@sitecore-jss/sitecore-jss-react';
+import { Image, Text } from '@sitecore-jss/sitecore-jss-react';
 import React from 'react';
 
 const HomeWhyUs = (props) => (
   <section className="why_section layout_padding">
-    <small>{JSON.stringify(props)}</small>
+    {/* <small>{JSON.stringify(props)}</small> */}
 
     <div className="container">
       <div className="heading_container heading_center">
@@ -12,66 +12,17 @@ const HomeWhyUs = (props) => (
         </h2>
       </div>
       <div className="why_container">
-        <div className="box">
-          <div className="img-box">
-            <img src="images/w1.png" alt="" />
+        {props.fields?.Speciality?.map((item) => (
+          <div key={item.id} className="box">
+            <div className="img-box">
+              <Image field={item.fields?.PromoImage.value} />
+            </div>
+            <div className="detail-box">
+              <h5>{item.fields?.PromoTitle.value}</h5>
+              <p>{item.fields?.PromoDescription.value}</p>
+            </div>
           </div>
-          <div className="detail-box">
-            <h5>Expert Management</h5>
-            <p>
-              Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam
-              numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa?
-              In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut
-              veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus
-              animi autem natus
-            </p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="img-box">
-            <img src="images/w2.png" alt="" />
-          </div>
-          <div className="detail-box">
-            <h5>Secure Investment</h5>
-            <p>
-              Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam
-              numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa?
-              In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut
-              veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus
-              animi autem natus
-            </p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="img-box">
-            <img src="images/w3.png" alt="" />
-          </div>
-          <div className="detail-box">
-            <h5>Instant Trading</h5>
-            <p>
-              Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam
-              numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa?
-              In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut
-              veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus
-              animi autem natus
-            </p>
-          </div>
-        </div>
-        <div className="box">
-          <div className="img-box">
-            <img src="images/w4.png" alt="" />
-          </div>
-          <div className="detail-box">
-            <h5>Happy Customers</h5>
-            <p>
-              Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam
-              numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa?
-              In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut
-              veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus
-              animi autem natus
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="btn-box">
         <a href=""> Read More </a>
