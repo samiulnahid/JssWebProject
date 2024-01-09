33009053579326cc1b61aa96c1d42a39caf33aa6
iksagor0 <iksagor0@gmail.com>
2024-01-09 18:30:06 +0600

    Event Component data bind

--- ui/src/components/EventComponent.js
+++ ui/src/components/EventComponent.js
@@ -1,15 +1,24 @@
-import { Text, withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
+import { DateField, RichText, Text, withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
 import React from 'react';
 
 const EventComponent = (props) => {
   console.log('🚀 ~ props:', props);
+  const { title, author, content, endDate, startDate } = props?.sitecoreContext?.route?.fields;
 
   return (
     <div>
-      <p>EventComponent Component</p>
-      <Text field={props.fields.title} />
+      <Text tag="h1" field={title} />
+      <Text tag="p" field={author} />
+      <RichText field={content} />
+      <h6>
+        Start Date: <DateField field={startDate} />
+      </h6>
+      <h6>
+        End Date: <DateField field={endDate} />
+      </h6>
+      {/* <Text field={props.fields.title} /> */}
     </div>
   );
 };
 
-export default withSitecoreContext(EventComponent);
+export default withSitecoreContext()(EventComponent);
