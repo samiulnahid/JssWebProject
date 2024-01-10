1f3b4baa1801d5dac10a7f8d96f7a401dc041cec
iksagor0 <iksagor0@gmail.com>
2024-01-10 12:48:57 +0600

    EventComponent style design

--- ui/src/components/EventComponent.js
+++ ui/src/components/EventComponent.js
@@ -1,23 +1,26 @@
-import { DateField, RichText, Text, withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
+import { RichText, Text, withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
 import React from 'react';
 
+const getDateFormatted = (date) => new Date(date).toLocaleString();
+
 const EventComponent = (props) => {
-  console.log('🚀 ~ props:', props);
   const { title, author, content, endDate, startDate } = props?.sitecoreContext?.route?.fields;
 
   return (
-    <div>
-      <Text tag="h1" field={title} />
-      <Text tag="p" field={author} />
-      <RichText field={content} />
-      <h6>
-        Start Date: <DateField field={startDate} />
-      </h6>
-      <h6>
-        End Date: <DateField field={endDate} />
-      </h6>
+    <section>
+      <div className="container mb-5 mt-4">
+        <Text tag="h1" field={title} />
+
+        <div className="date my-4">
+          <h6>Start Date: {getDateFormatted(startDate.value)} </h6>
+          <h6>End Date: {getDateFormatted(endDate.value)} </h6>
+        </div>
+
+        <Text tag="p" field={author} style={{ fontStyle: 'italic', fontSize: 14 }} />
+        <RichText field={content} className="mt-4" />
+      </div>
       {/* <Text field={props.fields.title} /> */}
-    </div>
+    </section>
   );
 };
 
