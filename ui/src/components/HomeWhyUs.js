a725bb871da5e5d15b25132f242ba021a1333bc7
iksagor0 <iksagor0@gmail.com>
2024-01-11 15:43:49 +0600

    HomeWhyUs Section Dynamic in Sitecore

--- ui/src/components/HomeWhyUs.js
+++ ui/src/components/HomeWhyUs.js
@@ -1,34 +1,34 @@
 import { Image, Text } from '@sitecore-jss/sitecore-jss-react';
 import React from 'react';
 
-const HomeWhyUs = (props) => (
-  <section className="why_section layout_padding">
-    {/* <small>{JSON.stringify(props)}</small> */}
-
-    <div className="container">
-      <div className="heading_container heading_center">
-        <h2>
-          <Text field={props.fields.Heading} />
-        </h2>
-      </div>
-      <div className="why_container">
-        {props.fields?.Speciality?.map((item) => (
-          <div key={item.id} className="box">
-            <div className="img-box">
-              <Image field={item.fields?.PromoImage.value} />
+const HomeWhyUs = (props) => {
+  return (
+    <section className="why_section layout_padding">
+      <div className="container">
+        <div className="heading_container heading_center">
+          <h2>
+            <Text field={props.fields.Heading} />
+          </h2>
+        </div>
+        <div className="why_container">
+          {props.fields?.Speciality?.map((item) => (
+            <div key={item.id} className="box">
+              <div className="img-box">
+                <Image field={item.fields?.PromoImage.value} />
+              </div>
+              <div className="detail-box">
+                <h5>{item.fields?.PromoTitle.value}</h5>
+                <p>{item.fields?.PromoDescription.value}</p>
+              </div>
             </div>
-            <div className="detail-box">
-              <h5>{item.fields?.PromoTitle.value}</h5>
-              <p>{item.fields?.PromoDescription.value}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="btn-box">
+          <a href=""> Read More </a>
+        </div>
       </div>
-      <div className="btn-box">
-        <a href=""> Read More </a>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default HomeWhyUs;
