7cbcf0a519c5aa670fe3b1213f8692241b433dad
iksagor0 <iksagor0@gmail.com>
2024-01-15 10:06:22 +0600

    Basic search functionality by js

--- ui/src/components/Events/EventsRootPage.js
+++ ui/src/components/Events/EventsRootPage.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import EventListWithPagination from './EventListWithPagination';
 import EventSearch from './EventSearch';
 
 const EventsRootPage = ({ fields }) => {
   const eventsList = fields?.items ? fields?.items : [];
   const filteredEvents = eventsList.filter((event) => event?.fields?.title);
+
+  const [events, setEvents] = useState(filteredEvents);
+
+  const handleSearch = (text) => {
+    if (window.searchWithDebounce) clearTimeout(window.searchWithDebounce);
+
+    window.searchWithDebounce = setTimeout(() => {
+      const searchRegex = new RegExp(text, 'i');
+      const filteredEvents = events.filter((book) => searchRegex.test(book?.fields?.title.value));
+
+      setEvents(filteredEvents);
+    }, 500);
+  };
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const searchQuery = searchParams.get('search');
+    if (searchQuery) handleSearch(searchQuery);
+  }, []);
 
   return (
     <section>
@@ -23,7 +44,7 @@ const EventsRootPage = ({ fields }) => {
         </div>
 
         <div className="event__list my-5">
-          <EventListWithPagination events={filteredEvents} />
+          <EventListWithPagination events={events} />
         </div>
       </div>
     </section>
